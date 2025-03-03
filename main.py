61ec1827acab9673830a9a0a1a7a4e62b6e40bc9
Engiavi <hackavi796@gmail.com>
2025-03-03 23:25:25 +0530

    added functionailty to identify the real world handwritten image to explicitly get the number

--- main.py
+++ main.py
@@ -4,32 +4,44 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import os
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data() 
+train_new_model = True
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1) 
+if train_new_model:
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data() 
 
-
-model = tf.keras.models.Sequential()
-
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) 
-
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) 
-model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax)) 
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1) 
 
 
-# model.compile(optimizer='adam',
-#                 loss='sparse_categorical_crossentropy',
-#                 metrics=['accuracy'])
+    model = tf.keras.models.Sequential()
 
-# model.fit(x_train, y_train, epochs=3)
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) 
 
-# model.save('MNISTify.keras')
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) 
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax)) 
 
-model = tf.keras.models.load_model('MNISTify.keras')
 
-loss,accuracy = model.evaluate(x_test, y_test)
+    model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
 
-print("loss: ",loss)
-print("accuracy: ",accuracy)
+    model.fit(x_train, y_train, epochs=3)
+
+    model.save('MNISTify.keras')
+else:
+    model = tf.keras.models.load_model('MNISTify.keras')
+
+image_number = 1
+while os.path.isfile('digits/digit{}.png'.format(image_number)):
+    try:
+        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print("The number is probably a {}".format(np.argmax(prediction)))
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+        image_number += 1
+    except:
+        print("Error reading image! Proceeding with next image...")
+        image_number += 1
